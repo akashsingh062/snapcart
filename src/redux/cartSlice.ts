8b27b3e6de91a5afe68664f49f8bb66f7f1989c0
akashsingh062 <akashsingh062.in@gmail.com>
2026-08-03 18:40:08 +0530

    feat: implement redux slice for cart state management and automatic total calculations

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -10,9 +10,9 @@ export interface ICartItem extends IGrocery {
 }
 interface ICartSlice {
   cartData: ICartItem[],
-  subTotal: 0,
-  deliveryFee: 40,
-  finalTotal: 40
+  subTotal: number,
+  deliveryFee: number,
+  finalTotal: number
 }
 const initialState: ICartSlice = {
   cartData: [],
@@ -33,11 +33,13 @@ const cartSlice = createSlice({
       } else {
         state.cartData.push(action.payload);
       }
+      cartSlice.caseReducers.calculateTotal(state)
     },
     removeFromCart: (state, action: PayloadAction<string>) => {
       state.cartData = state.cartData.filter(
         (item) => item._id !== action.payload,
       );
+      cartSlice.caseReducers.calculateTotal(state)
     },
     updateQuantity: (
       state,
@@ -52,15 +54,18 @@ const cartSlice = createSlice({
           );
         }
       }
+      cartSlice.caseReducers.calculateTotal(state)
     },
     calculateTotal:(state)=>{
       state.subTotal = state.cartData.reduce(
-        (total, item)=> total + item.price * item.quantity,
+        (total, item)=> total + Number(item.price) * item.quantity,
         0
       )
+      state.deliveryFee = state.subTotal > 4000 ? 0:40
+      state.finalTotal = state.subTotal + state.deliveryFee
     }
   },
 });
 
-export const { addToCart, removeFromCart, updateQuantity } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateQuantity, calculateTotal } = cartSlice.actions;
 export default cartSlice.reducer;
