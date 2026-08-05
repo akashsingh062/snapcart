8191a289eac89fe1a7c3fad2e8de5f9c973f2208
akashsingh062 <akashsingh062.in@gmail.com>
2026-08-05 22:23:27 +0530

    feat: create user Mongoose model with geolocation indexing support

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,47 +1,76 @@
 import mongoose from "mongoose";
 
 interface IUser {
-    _id?:mongoose.Types.ObjectId
-    name:string
-    email:string
-    password:string
-    mobile?:string
-    role:"user" | "deliveryBoy" | "admin"
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  mobile?: string;
+  role: "user" | "deliveryBoy" | "admin";
+  image?: string;
+  location: {
+    type: {
+        type: StringConstructor;
+        enum: string[];
+        default: string;
+    };
+    coordinates: {
+        type: NumberConstructor[];
+        default: number[];
+    };
 }
-const userSchema = new mongoose.Schema<IUser>({
-   name:{
-    type:String,
-    required:true,
-    trim:true
-   },
-   email:{
-    type:String,
-    required:true,
-    unique:true,
-    trim:true
-   },
-   password:{
-    type:String,
-    required:true,
-    trim:true
-   },
-   mobile:{
-    type:String,
-    required:false,
-    trim:true
-   },
-   role:{
-    type:String,
-    required:true,
-    enum:["user","deliveryBoy","admin"],
-    default:"user"
-   }
-},{
-    timestamps:true
-})
+}
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      enum: ["user", "deliveryBoy", "admin"],
+      default: "user",
+    },
+    image: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        default: "Point",
+      },
+      coordinates: {
+        type: [Number],
+        default: [0, 0],
+      },
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+userSchema.index({location:"2dsphere"})
+const User = mongoose.models.User || mongoose.model("User", userSchema, "user");
 
-
-const User = mongoose.models.User || mongoose.model("User", userSchema, "user")
-
-
-export default User
+export default User;
