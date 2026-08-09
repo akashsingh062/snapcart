60b61b2cd85460509d1084d49e2c586d9985349e
akashsingh062 <akashsingh062.in@gmail.com>
2026-08-09 20:02:02 +0530

    feat: add real-time location tracking via Socket.io and implement HeroSection and dynamic navigation components

---
+++ src/components/GeoUpdater.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { getSocket } from "@/lib/socket";
+import { RootState } from "@/redux/store";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+const GeoUpdater = () => {
+  const { userData } = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    if (!userData?._id) return;
+    const socket = getSocket();
+    socket.emit("identity", userData._id);
+
+    if (!navigator.geolocation) return;
+
+    const watcher = navigator.geolocation.watchPosition(
+      (pos) => {
+        const lat = pos.coords.latitude;
+        const lng = pos.coords.longitude;
+        socket.emit("update-location", {
+          userId: userData._id,
+          latitude: lat,
+          longitude: lng,
+        });
+      },
+      (error) => {
+        console.error("GeoUpdater position error:", error.message || error);
+      },
+      {
+        enableHighAccuracy: true,
+      }
+    );
+
+    return () => {
+      navigator.geolocation.clearWatch(watcher);
+    };
+  }, [userData]);
+
+  return null;
+};
+
+export default GeoUpdater;
